4bf74ffe26e83f0f827fc2c95b216298814a9137
angelxescomx08 <jhernandezr1313@alumno.ipn.mx>
2023-03-18 12:04:42 -0600

    diseño del login

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,7 +1,55 @@
-import React from 'react'
+import { Grid, TextField, Typography } from '@mui/material'
 
 export const LoginPage = () => {
     return (
-        <div>LoginPage</div>
+        <Grid
+            container
+            spacing={0}
+            direction="column"
+            alignItems="center"
+            justifyContent="center"
+            sx={{
+                minHeight: '100vh',
+                backgroundColor: 'primary.main',
+                padding: 4
+            }}
+        >
+            <Grid
+                item
+                className='box-shadow'
+                xs={3}
+                sx={{
+                    backgroundColor: 'white',
+                    padding: 3,
+                    borderRadius: 2
+                }}
+            >
+                <Typography variant='h5' sx={{ marginBottom: 2 }}>
+                    Login
+                </Typography>
+
+                <form>
+                    <Grid container>
+                        <Grid item xs={12} sx={{ marginTop: 2 }}>
+                            <TextField
+                                label="Correo"
+                                placeholder='correo@gmail.com'
+                                fullWidth
+                            />
+
+                        </Grid>
+
+                        <Grid item xs={12} sx={{ marginTop: 2 }}>
+                            <TextField
+                                label="Contraseña"
+                                type="password"
+                                placeholder='Contraseña'
+                                fullWidth
+                            />
+                        </Grid>
+                    </Grid>
+                </form>
+            </Grid>
+        </Grid>
     )
 }
