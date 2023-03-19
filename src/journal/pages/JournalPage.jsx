a9a3bf862b6261bc66f6cacc95640b728af47ad6
angelxescomx08 <jhernandezr1313@alumno.ipn.mx>
2023-03-18 19:10:38 -0600

    vista de nada seleccionado

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,12 +1,11 @@
-import { Typography } from "@mui/material"
+
 import { JournalLayout } from "../layout/JournalLayout"
+import { NothingSelectedView } from "../views"
 
 export const JournalPage = () => {
     return (
         <JournalLayout>
-            <Typography>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil, nostrum tempora? Aliquid repellendus odit ex debitis veritatis vitae. Maiores omnis asperiores aliquid ab nobis assumenda quisquam consectetur dolorum. Qui, quos?
-            </Typography>
+            <NothingSelectedView/>
         </JournalLayout>
     )
 }
