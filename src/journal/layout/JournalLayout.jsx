74d1b57a2f7231e58119ec12803e57f15f2a69d7
angelxescomx08 <jhernandezr1313@alumno.ipn.mx>
2023-03-18 18:15:45 -0600

    Navbar

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,9 +1,12 @@
 import { Box } from "@mui/system"
+import { Navbar } from "../components"
 
+const drawerWidth = 240
 
 export const JournalLayout = ({ children }) => {
     return (
         <Box sx={{ display: 'flex' }}>
+            <Navbar drawerWidth={drawerWidth} />
             <Box
                 component='main'
                 sx={{
--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,8 +1,6 @@
 import { Typography } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
 
-const drawerWidth = 240
-
 export const JournalPage = () => {
     return (
         <JournalLayout>
