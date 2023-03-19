dc2750ff927c011669d090737d900dd437a35e27
angelxescomx08 <jhernandezr1313@alumno.ipn.mx>
2023-03-18 18:05:26 -0600

    Journal layout y JournalPage

---
+++ src/journal/layout/JournalLayout.jsx
@@ -0,0 +1,18 @@
+import { Box } from "@mui/system"
+
+
+export const JournalLayout = ({ children }) => {
+    return (
+        <Box sx={{ display: 'flex' }}>
+            <Box
+                component='main'
+                sx={{
+                    flexGrow: 1,
+                    p: 3
+                }}
+            >
+                {children}
+            </Box>
+        </Box>
+    )
+}
--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,7 +1,14 @@
+import { Typography } from "@mui/material"
+import { JournalLayout } from "../layout/JournalLayout"
 
+const drawerWidth = 240
 
 export const JournalPage = () => {
     return (
-        <div>JournalPage</div>
+        <JournalLayout>
+            <Typography>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil, nostrum tempora? Aliquid repellendus odit ex debitis veritatis vitae. Maiores omnis asperiores aliquid ab nobis assumenda quisquam consectetur dolorum. Qui, quos?
+            </Typography>
+        </JournalLayout>
     )
 }
